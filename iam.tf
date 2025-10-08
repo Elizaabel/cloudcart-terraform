2178295317ca7af0e3d411be20689c0b7b81aa05
Elizaabel <liztee10@gmail.com>
2025-10-07 23:59:47 -0500

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,36 @@
+# IAM Role for EC2
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "cloudcart-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+# Attach managed policies for SSM and CloudWatch logs
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "cw_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "cloudcart-instance-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
